e2ecf1a7bc1915d6fb91bd537e870200cb6c75de
Matheusamato15 <matheusamato15@gmail.com>
2022-03-21 21:46:32 -0300

    validacoes terminadas

--- src/main.js
+++ src/main.js
@@ -16,18 +16,34 @@ function validarCampos(){
     var valorInputEmail = inputEmail.value; 
 
     var usuario = valorInputEmail.substring(0, valorInputEmail.indexOf("@"));
-    var dominio = valorInputEmail.substring(valorInputEmail.indexOf("@")+1, valorInputEmail.lenght);
+    var dominio = valorInputEmail.substring(valorInputEmail.indexOf("@")+1, valorInputEmail.length);
+
+    console.log(dominio);
 
     if(valorInputNome == ""){
         alert("Erro no envio: Insira seu nome");
         inputNome.focus();
         return false;
-    } if(valorInputTextArea == ""){
+
+    }if(valorInputTextArea == ""){
         alert("Erro no envio: Insira uma mensagem");
         inputTextArea.focus();
         return false;
-    } if(usuario.lenght < 3){
-        alert("Erro no envio: Endereço de mail inválido");
+
+    }if(valorInputEmail.indexOf("@") > -1 == false){
+        alert("Seu email não segue a estrutura básica: usuario@dominio.com");
+        return false;
+
+    }if(usuario.length + 1 <= 32 &&
+        usuario.length < 1 &&
+        dominio.length + 1 <= 16 &&
+        dominio. length < 1){
+        alert("Erro no envio: Endereço de email inválido");
+        inputEmail.focus();
+        return false;
+        
+    }else{
+        alert(`Obrigado pelo contato, ${valorInputNome}!`);
     }
 
 }
