ad7b91ea98838d1662e5ed1efc5f734c967d3c52
Matheusamato15 <matheusamato15@gmail.com>
2022-03-22 17:44:26 -0300

    ultimos ajustes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 var inputSubmit = document.querySelector("#inputSubmit"); //salvando botão de envio do html
-var valorInputSubmit = inputSubmit.value;
 
-inputSubmit.onclick=validarCampos; //chamando a função para validar os campos ao pressionar do botão
+inputSubmit.onclick = validarCampos; //chamando a função para validar os campos ao pressionar do botão
 
 
-function validarCampos(){ //função para validar campos
-    
+function validarCampos() { //função para validar campos
+
     var inputNome = document.querySelector("#inputNome"); //salvando input nome do html
     var valorInputNome = inputNome.value;
 
@@ -13,35 +12,38 @@ function validarCampos(){ //função para validar campos
     var valorInputTextArea = inputTextArea.value;
 
     var inputEmail = document.querySelector("#inputEmail"); //salvando input email do html
-    var valorInputEmail = inputEmail.value; 
+    var valorInputEmail = inputEmail.value;
 
     //dividindo o email em parte do usuário e parte do domínio
-    var usuario = valorInputEmail.substring(0, valorInputEmail.indexOf("@")); 
-    var dominio = valorInputEmail.substring(valorInputEmail.indexOf("@")+1, valorInputEmail.length);
+    var usuario = valorInputEmail.substring(0, valorInputEmail.indexOf("@"));
+    var dominio = valorInputEmail.substring(valorInputEmail.indexOf("@") + 1, valorInputEmail.length);
 
-    if(valorInputNome == ""){ //validação do nome
+    if (valorInputNome == "") { //validação do nome
         alert("Erro no envio: Insira seu nome");
         inputNome.focus();
         return false;
 
-    }if(valorInputTextArea == ""){ //validação da mensagem
+    }
+    if (valorInputTextArea == "") { //validação da mensagem
         alert("Erro no envio: Insira uma mensagem");
         inputTextArea.focus();
         return false;
 
-    }if(valorInputEmail.indexOf("@") > -1 == false){ //validação se email possui @
+    }
+    if (valorInputEmail.indexOf("@") > -1 == false || dominio.indexOf(".com") > -1 == false) { //validação se email possui @ e .com
         alert("Seu email não segue a estrutura básica: usuario@dominio.com");
         return false;
 
-    }if(usuario.length + 1 <= 32 && //validação do email
-        usuario.length < 1 &&
-        dominio.length + 1 <= 16 &&
-        dominio. length < 1){
+    }
+    if (usuario.length > 32 || //validação do email
+        usuario.length < 1 ||
+        dominio.length > 16 ||
+        dominio.length < 1) {
         alert("Erro no envio: Endereço de email inválido");
         inputEmail.focus();
         return false;
-        
-    }else{ //exibindo mensagem na tela
+
+    } else { //exibindo mensagem na tela
         alert(`Obrigado pelo contato, ${valorInputNome}!`);
     }
 
